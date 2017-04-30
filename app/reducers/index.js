2a023178fe6182cd6278cb4c14f27704b13e2115
Pimpila <pimpila@gmail.com>
2017-04-29 20:14:41 -0400

    add all async reqs to onButtonPress in home

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux'
-import  { setRecipesInStore, setSearchTermsInStore, setSuperFrugal } from './recipes'
+import  { setRecipesInStore, setSearchTermsInStore, setFrugalSearchTermsInStore, setSuperFrugal } from './recipes'
 
 // this is your root reducer. keys you pass to combineReducers ends up being the props on your global store
 // left off here: recipes is set to undefined, not an empty array
 export default combineReducers({
   recipes: setRecipesInStore,
   searchTerms: setSearchTermsInStore,
+  frugalSearchTerms: setFrugalSearchTermsInStore,
   superRecipe: setSuperFrugal
 })
